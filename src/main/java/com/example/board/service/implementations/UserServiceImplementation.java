1528453790f2a4146a16d1902ef14c00703fa18e
ahndb <asi0000@nate.com>
2024-04-03 15:50:20 +0900

    240403 15:50 Update try catch

--- src/main/java/com/example/board/service/implementations/UserServiceImplementation.java
+++ src/main/java/com/example/board/service/implementations/UserServiceImplementation.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.example.board.dto.response.ResponseDto;
 import com.example.board.repository.UserRepository;
 import com.example.board.dto.response.user.GetUserResponseDto;
+import com.example.board.entity.UserEntity;
 import com.example.board.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,14 +21,37 @@ public class UserServiceImplementation implements UserService {
   public ResponseEntity<? super GetUserResponseDto> getUser(String email) {
 
     try {
-      
-      return ResponseDto.notExistUser();
+      // 1. User 테이블에서 email에 해당하는 유저 조회
+      // SELECT * FROM user WHERE email = :email
+      // findByEmail(email)
+      // (email -> (조회 결과 인스턴스))
+      UserEntity userEntity = userRepository.findByEmail(email);
+
+          // 2. 조회 결과에 따라 반환 결정
+    // 1) false이면 존재하지 않는 유저 응답처리 X
+    // 2) null 이면 존재하지 않는 유저 응답처리
+    if (userEntity == null) return ResponseDto.notExistUser();
+
+    // 3. 조회 결과 데이터를 성공 응답
+      return GetUserResponseDto.success(userEntity);
 
     } catch (Exception exception) {
-      exception.printStackTrace();
+      // 1-1. 조회 처리 중 데이터베이스관련 예외가 발생하면 데이터베이스 에러 응답처리
+      exception.printStackTrace(); // 터미널에 어떤 예외가 발생했는지 알려줌
       return ResponseDto.databaseError();
-
     }
+
+  
+
+    // try {
+      
+    //   return ResponseDto.notExistUser();
+
+    // } catch (Exception exception) {
+    //   exception.printStackTrace();
+    //   return ResponseDto.databaseError();
+
+    // }
 
     // return GetUserResponseDto.success("email@email.com", "홍길동", "null");
   }
